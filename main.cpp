95c772cfee4f0a9baaa1572995fcd7bb5141fab0
unknown <diego.roymen@gmail.com>
2019-04-23 20:04:54 +0200

    Programa de pruebas + imagen resultado

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
-#include "rgbcolor.h"
 #include "pngimage.h"
+#include "rgbcolor.h"
 
 void imageInfo(PNGImage& image) {
     std::cout << "Imagen de " << image.width << "x" << image.height
               << std::endl;
-    for (int x = 0; x < image.width; x++) {
-        for (int y = 0; y < image.height; y++) {
+    for (int y = 0; y < image.height; y++) {
+        for (int x = 0; x < image.width; x++) {
             std::cout << "Pixel (" << (int)x << ", " << (int)y << "): "
                       << "r = " << int(image.get_pixel(x, y)->r)
                       << ", g = " << int(image.get_pixel(x, y)->g)
-                      << ", b = " << int(image.get_pixel(x, y)->b)
-                      << std::endl;
+                      << ", b = " << int(image.get_pixel(x, y)->b) << std::endl;
         }
     }
 }
 
 int main() {
     PNGImage img;
-    RGBColor white(255, 255, 255);
+    RGBColor green(0, 255, 0);
     if (img.read_png_file("png/test_tom.png")) {
-        // Píxeles de la diagonal en blanco
-        for (int i = 0; i < img.width; i++) {
-            img.set_pixel(i, i, &white);
-        }
+        // Mostrar información
         imageInfo(img);
+        // Píxeles de la diagonal en verde
+        for (int i = 0; i < img.width; i++) {
+            img.set_pixel(i, i, &green);
+        }
+        img.write_png_file("png/test_tom_other.png");
     } else {
         std::cout << "Error al leer la imagen" << std::endl;
     }
