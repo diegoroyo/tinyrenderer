9e1afa92c6efebd9d3e7073d0f3371c0cfc67dce
unknown <diego.roymen@gmail.com>
2019-07-10 14:40:40 +0200

    Lineas con el alg. Bresenham de enteros

--- main.cpp
+++ main.cpp
@@ -2,34 +2,49 @@
 #include "pngimage/pngimage.h"
 #include "pngimage/rgbcolor.h"
 
+void line(int x0, int y0, int x1, int y1, PNGImage& image,
+          const RGBColor& color) {
+    bool steep = false;
+    if (std::abs(y0 - y1) > std::abs(x0 - x1)) {
+        // más alta que ancha
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) {
+        // dibujar de derecha a izquierda
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    // multiplicar todos los errores por (2*dx)
+    // derror pasa de abs(dy/dx) a abs(dy)*2
+    int derror2 = std::abs(dy)*2;
+    int error2 = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++) {
+        if (steep) {
+            image.set_pixel(y, x, color);
+        } else {
+            image.set_pixel(x, y, color);
+        }
+        error2 += derror2;
+        // necesario comprobar por .5 * 2 * dx = dx
+        if (error2 > dx) {
+            y += (y1 > y0) ? 1 : -1;
+            // restar 1 * 2 * dx = 2*dx
+            error2 -= 2*dx;
+        }
+    }
+}
+
 int main() {
-    // Crear una imagen desde 0 y guardarla (gradient.png)
-    PNGImage newImg(256, 256);
-    for (int y = 0; y < newImg.height; y++) {
-        for (int x = 0; x < newImg.width; x++) {
-            RGBColor color(y * 255 / newImg.height, x * 255 / newImg.width, 0);
-            newImg.set_pixel(x, y, color);
-        }
-    }
-    newImg.write_png_file("pngimage/gradient.png");
-
-    // Leer gradient.png y generar su negativo
-    PNGImage readImg;
-    if (readImg.read_png_file("pngimage/gradient.png")) {
-        RGBColor readColor;
-        for (int y = 0; y < readImg.height; y++) {
-            for (int x = 0; x < readImg.width; x++) {
-                readImg.get_pixel(x, y, readColor);
-                readColor.r = 255 - readColor.r;
-                readColor.g = 255 - readColor.g;
-                readColor.b = 255 - readColor.b;
-                readImg.set_pixel(x, y, readColor);
-            }
-        }
-        readImg.write_png_file("pngimage/gradient_negative.png");
-    } else {
-        std::cout << "Error al leer la imagen" << std::endl;
-    }
-
+    PNGImage image(100, 100, RGBColor::Black);
+    line(13, 20, 80, 40, image, RGBColor::White);
+    line(20, 13, 40, 80, image, RGBColor::Red);
+    line(80, 40, 13, 20, image, RGBColor::Red);
+    image.flip_vertically();
+    image.write_png_file("images/output.png");
     return 0;
 }
