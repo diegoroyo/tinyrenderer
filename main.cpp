331ce3f6a2f76dcbba436bb97be505dfa9c8c90c
unknown <diego.roymen@gmail.com>
2019-07-10 17:48:23 +0200

    Renderizado wireframe

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "pngimage/pngimage.h"
 #include "pngimage/rgbcolor.h"
+#include "model.h"
 
 void line(int x0, int y0, int x1, int y1, PNGImage& image,
           const RGBColor& color) {
@@ -20,7 +21,7 @@ void line(int x0, int y0, int x1, int y1, PNGImage& image,
     int dy = y1 - y0;
     // multiplicar todos los errores por (2*dx)
     // derror pasa de abs(dy/dx) a abs(dy)*2
-    int derror2 = std::abs(dy)*2;
+    int derror2 = std::abs(dy) * 2;
     int error2 = 0;
     int y = y0;
     for (int x = x0; x <= x1; x++) {
@@ -34,16 +35,35 @@ void line(int x0, int y0, int x1, int y1, PNGImage& image,
         if (error2 > dx) {
             y += (y1 > y0) ? 1 : -1;
             // restar 1 * 2 * dx = 2*dx
-            error2 -= 2*dx;
+            error2 -= 2 * dx;
         }
     }
 }
 
-int main() {
-    PNGImage image(100, 100, RGBColor::Black);
-    line(13, 20, 80, 40, image, RGBColor::White);
-    line(20, 13, 40, 80, image, RGBColor::Red);
-    line(80, 40, 13, 20, image, RGBColor::Red);
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <model_name>" << std::endl;
+        return 1;
+    }
+
+    Model model(argv[1]);
+    const int width = 800, height = 800;
+    PNGImage image(width, height, RGBColor::Black);
+
+    // Renderizado básico
+    for (int i = 0; i < model.nfaces(); i++) {
+        std::vector<int> face = model.face(i);
+        for (int j = 0; j < face.size(); j++) {
+            Vec3f v0 = model.vert(face[j]);
+            Vec3f v1 = model.vert(face[(j + 1) % face.size()]);
+            int x0 = (v0.x + 1.0) * width / 2.0;
+            int x1 = (v1.x + 1.0) * width / 2.0;
+            int y0 = (v0.y + 1.0) * height / 2.0;
+            int y1 = (v1.y + 1.0) * height / 2.0;
+            line(x0, y0, x1, y1, image, RGBColor::White);
+        }
+    }
+
     image.flip_vertically();
     image.write_png_file("images/output.png");
     return 0;
