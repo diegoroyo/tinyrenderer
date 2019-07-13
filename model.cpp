af33c974f22d9206673788765d357cdd9f7c230e
diegoroyo <diego.roymen@gmail.com>
2019-07-13 18:48:24 +0200

    Lectura de vertices texture/normal y diffuse

--- model.cpp
+++ model.cpp
@@ -4,14 +4,27 @@
 #include <iostream>
 #include <sstream>
 
-Model::Model(const char *filename) : verts(), faces() {
+void Model::load_texture(const char* filename) {
+    std::string texturename(filename);
+    size_t pos = texturename.find_last_of(".");
+    if (pos == std::string::npos) {
+        std::cerr << "Invalid obj filename" << std::endl;
+    } else {
+        texturename = texturename.substr(0, pos) + "_diffuse.png";
+        diffuseMap.read_png_file(texturename.c_str());
+        diffuseMap.flip_vertically();
+    }
+}
+
+Model::Model(const char* filename) : verts(), faces(), uvs(), norms() {
     // Apertura
     std::ifstream is(filename);
     if (!is.is_open()) {
         std::cerr << "Can't open file " << filename << std::endl;
         is.close();
     }
-
+    // Textura (almacenada en diffuseMap)
+    load_texture(filename);
     // Lectura
     std::string line;
     while (!is.eof()) {
@@ -24,16 +37,32 @@ Model::Model(const char *filename) : verts(), faces() {
             // Leer vertice y añadir
             for (int i = 0; i < 3; i++) iss >> v.raw[i];
             verts.push_back(v);
+        } else if (line.compare(0, 3, "vt ") == 0) {
+            iss >> ctrash >> ctrash;  // leer "vt"
+            Vec2f v;
+            // Leer textura y añadir
+            for (int i = 0; i < 2; i++) iss >> v.raw[i];
+            uvs.push_back(v);
+        } else if (line.compare(0, 3, "vn ") == 0) {
+            iss >> ctrash >> ctrash;  // leer "vn"
+            Vec3f v;
+            // Leer normal y añadir
+            for (int i = 0; i < 3; i++) iss >> v.raw[i];
+            norms.push_back(v);
         } else if (line.compare(0, 2, "f ") == 0) {
             iss >> ctrash;  // leer "f"
-            std::vector<int> f;
+            std::vector<Vec3i> f;
             // Leer cara y añadir
-            int itrash, idx;
-            // leer "idx/_/_", el primer numero hace referencia
-            // a un vertice que forma la cara
-            while (iss >> idx >> ctrash >> itrash >> ctrash >> itrash) {
+            int idx, iduv, idnorm;
+            // leer "idx/iduv/idnorm"
+            // idx -> id de vertice (v)
+            // iduv -> id de texture vertex (vt)
+            // idnorm -> id de normal vertex (vn)
+            while (iss >> idx >> ctrash >> iduv >> ctrash >> idnorm) {
                 idx--;  // en formato .obj los indices empiezan en 1, no 0
-                f.push_back(idx);
+                iduv--;
+                idnorm--;
+                f.push_back(Vec3i(idx, iduv, idnorm));
             }
             faces.push_back(f);
         }
@@ -55,5 +84,18 @@ Vec3f Model::vert(int i) {
 }
 
 std::vector<int> Model::face(int idx) {
-    return faces[idx];
+    std::vector<int> verts;
+    for (Vec3i face : faces[idx]) verts.push_back(face.ivert);
+    return verts;
+}
+
+Vec2f Model::uv(int iface, int nvert) {
+    return uvs[faces[iface][nvert].iuv];
+}
+
+RGBColor Model::diffuse(Vec2f uv) {
+    RGBColor color;
+    diffuseMap.get_pixel((int)(uv.x * diffuseMap.width),
+                         (int)(uv.y * diffuseMap.height), color);
+    return color;
 }
--- model.h
+++ model.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include "geometry.h"
+#include "pngimage/pngimage.h"
+#include "pngimage/rgbcolor.h"
 
 // Modelos en formato wavefront .obj
 // más info: https://en.wikipedia.org/wiki/Wavefront_.obj_file
@@ -9,7 +11,12 @@
 class Model {
    private:
     std::vector<Vec3f> verts;
-    std::vector<std::vector<int>> faces;
+    std::vector<std::vector<Vec3i>> faces;
+    std::vector<Vec2f> uvs;
+    std::vector<Vec3f> norms;
+    PNGImage diffuseMap;
+
+    void load_texture(const char *filename);
 
    public:
     Model(const char *filename);
@@ -18,4 +25,6 @@ class Model {
     int nfaces();
     Vec3f vert(int i);
     std::vector<int> face(int idx);
+    Vec2f uv(int iface, int nvert);
+    RGBColor diffuse(Vec2f uv);
 };
